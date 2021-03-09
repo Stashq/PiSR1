3736f6de884a39dc0aeaf9ef0e1fde55ecbccef2
Piotr Rarus <piotr.rarus@softwarehut.com>
2021-03-09 13:14:01 +0100

    // WIP: Ratings EDA

--- src/util/plot.py
+++ src/util/plot.py
@@ -17,14 +17,14 @@ class Plot:
         fig = go.Figure()
 
         for loss, name in zip(losses, names):
-            trace = go.Scatter(
+            scatter = go.Scatter(
                 x=list(range(0, len(loss))),
                 y=loss,
                 mode='lines',
                 name=name
             )
 
-            fig.add_trace(trace)
+            fig.add_trace(scatter)
 
         fig.update_layout(
             title='Convergence',
@@ -33,5 +33,63 @@ class Plot:
         )
 
         fig.show(renderer='svg')
-        # img_bytes = fig.to_image(format='png', scale=5)
-        # return Image(img_bytes)
+
+    def relplot(
+        self,
+        x: List[float],
+        y: List[float],
+        marker_color: List[float],
+        title: str,
+        names: List[str]
+    ):
+        fig = go.Figure()
+
+        scatter = go.Scatter(
+            x=x,
+            y=y,
+            marker_color=marker_color,
+            mode='markers',
+            marker=dict(
+                opacity=0.5,
+                showscale=True,
+                colorbar=dict(
+                    title=names[2]
+                )
+            )
+        )
+
+        fig.add_trace(scatter)
+
+        fig.update_layout(
+            title=title,
+            xaxis_title=names[0],
+            yaxis_title=names[1]
+        )
+
+        fig.show(renderer='svg')
+
+    def histogram2d(
+        self,
+        x: List[float],
+        y: List[float],
+        title: str,
+        names: List[str]
+    ):
+        fig = go.Figure()
+
+        histogram = go.Histogram2d(
+            x=x,
+            y=y,
+            colorbar=dict(
+                title=names[2]
+            )
+        )
+        fig.add_trace(histogram)
+
+        fig.update_layout(
+            title=title,
+            xaxis_title=names[0],
+            yaxis_title=names[1]
+        )
+
+        fig.show(renderer='svg')
