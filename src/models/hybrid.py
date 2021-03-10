8f5e66ae97810c9577c6be8ff66162c7aea800d7
Stanisław Straburzyński <sstraburzynski@gmail.com>
2021-03-10 11:43:26 +0100

    zrobione z Rarusem

---
+++ src/models/hybrid.py
@@ -0,0 +1,62 @@
+from abc import ABC
+from typing import List, Tuple
+
+import numpy as np
+from src.models.recommender import RecommenderSystem
+
+class Hybrid(RecommenderSystem):
+
+    def __init__(self):
+        super(RecommenderSystem, self).__init__()
+        self.MAX_RATING = 5
+        self.MIN_RATING = 0
+
+    def predict(self, user_id: int) -> List[int]:
+        """
+        Predicts ranking of movies to watch for a user.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+
+        Returns
+        -------
+        List[int]
+            List of movies ids. Best recommendations first.
+        """
+        pass
+
+    def predict_score(self, user_id: int, movie_id: int) -> float:
+        """
+        Predicts score for a given movie that a user would give.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+        movie_id : int
+            Movie's id from the data set.
+
+        Returns
+        -------
+        float
+            Predicted movie's score in range [0, 5].
+        """
+        pass
+
+    def predict_scores(self, user_id: int) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Predicts scores for all the movies, that a user would give.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+
+        Returns
+        -------
+        Tuple[np.ndarray, np.ndarray]:
+            Ranked movies with their scores.
+        """
+        pass
